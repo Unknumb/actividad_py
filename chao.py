3c4ff81e9c75fd8127660e098a2edb073474602a
Alvaro Uribe <167482345+Unknumb@users.noreply.github.com>
2024-06-17 16:42:36 -0400

    mas cambios

--- chao.py
+++ chao.py
@@ -1,9 +1,25 @@
-def juany():
-    print("Hola, soy Juany")
-    print("¿Cómo estás?")
 
-def olanda(oli):
-    print("Hola, soy Olanda")
-    print("¿Cómo estás?")
-    print(oli)
+encabezado=['Trabajador','Cargo','Sueldo Bruto','Desc. Salud','Desc. AFP','Liquido a pagar'];
+trabajadores=[[
+
+]];
+cargo=['CEO','Analista de Datos','Desarrollador']
+
+def ingresaTrabajador():
+    nombre=input("Ingrese su nombre y apellido: ");
+    trabajadores.append(nombre);
+    cargo1=input("Ingrese su cargo: ");
+    if cargo1==cargo:
+        print("cargo ingresado.")
+        trabajadores.append(cargo);
+        print("Cargo no identificado,vuelva a intentarlo")
+    sueldo=int(input("Ingrese su sueldo bruto: "));
+    trabajadores.append(sueldo);
+   
+   
+def listarTrabajadores():
+    trabajadores()
     
+
+
+
